17f2241e147ab3c050fcc75a49fb31394ad4b6da
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-03-08 16:26:33 +0900

    [level 3] Title: N으로 표현, Time: 16.95 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,23 @@
+class Solution {
+    int answer=Integer.MAX_VALUE;
+    public int solution(int N, int number) {
+        dfs(0,0,N,number);
+        if(answer==Integer.MAX_VALUE) return -1;
+        return answer;
+    }
+    public void dfs(int count,int value,int N,int number){
+        if(count>8) return;
+        if(value==number){
+            answer=Math.min(answer,count);
+            return;
+        }
+        int newN=0;
+        for(int i=0;i<8;i++){
+            newN=newN*10+N;
+            dfs(count+1+i,value+newN,N,number);
+            dfs(count+1+i,value-newN,N,number);
+            dfs(count+1+i,value*newN,N,number);
+            dfs(count+1+i,value/newN,N,number);
+        }
+    }
+}
