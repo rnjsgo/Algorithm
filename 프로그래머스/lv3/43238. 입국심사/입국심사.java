2ab35854a9a691a9e0c66b496b0dd2f066d86438
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-03-18 14:08:21 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        int minTime=Integer.MAX_VALUE;
+        long answer=Long.MAX_VALUE;
+        for(int t:times)
+            minTime=Math.min(minTime,t);    
+        long start=1;
+        long end=(long)minTime*n;
+        long mid=(start+end)/2;
+        
+        while(start<=end){
+            mid=(start+end)/2;
+            long possibleNum=0;
+            for(int t:times)
+                possibleNum+=mid/t;
+            if(possibleNum<n)
+                start=mid+1;
+            else{
+                end=mid-1;
+                answer=Math.min(answer,mid);
+            }
+        }
+        return answer;
+    }
+}
