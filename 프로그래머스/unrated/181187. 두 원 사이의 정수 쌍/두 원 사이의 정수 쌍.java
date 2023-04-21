40053b779e88c8d707fdd89ce489fd2ac1ddbce4
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-04-21 14:00:25 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 22.85 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        long R1= (long)Math.pow(r1,2);
+        long R2= (long)Math.pow(r2,2);
+        
+        for(int i=1;i<r2;i++){
+            double countR1=Math.sqrt(R1-Math.pow(i,2));
+            double countR2=Math.sqrt(R2-Math.pow(i,2));
+            answer+=4*((int)countR2-(int)countR1);
+            if(countR1%1==0&&countR1!=0) answer+=4;
+        }
+        answer+=(r2-r1+1)*4;
+        return answer;
+    }
+}
