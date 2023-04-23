50ff05f17a77bff848be98e1189bd1e5e476ab08
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-04-23 13:10:45 +0900

    [unrated] Title: 인사고과, Time: 79.17 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/README.md
+++ 프로그래머스/unrated/152995. 인사고과/README.md
@@ -1,10 +1,10 @@
 # [unrated] 인사고과 - 152995 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995) 
 
 ### 성능 요약
 
-메모리: 125 MB, 시간: 72.84 ms
+메모리: 123 MB, 시간: 79.17 ms
 
 ### 구분
 
--- 프로그래머스/unrated/152995. 인사고과/인사고과.java
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.java
@@ -2,7 +2,6 @@ import java.util.*;
 class Solution {
     public int solution(int[][] scores) {
         int answer = 1;
-        int wanhoScore=scores[0][0]+scores[0][1];
         int[] wanho=scores[0];
         
         Arrays.sort(scores,(a,b)->a[0]==b[0]?a[1]-b[1]:b[0]-a[0]);
@@ -11,9 +10,9 @@ class Solution {
         for(int i=0;i<scores.length;i++){
             if(scores[i][1]<max){
                 if(scores[i][0]==wanho[0]&&scores[i][1]==wanho[1]) return -1;
-                scores[i][0]=-100001;
+                continue;
             }
-            if(scores[i][0]+scores[i][1]>wanhoScore) answer++;
+            if(scores[i][0]+scores[i][1]>wanho[0]+wanho[1]) answer++;
             max=Math.max(max,scores[i][1]);
         }
         
