f270a1e2d25681dad611a67d558db4e9461dd938
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-05-05 11:19:56 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,23 +1,20 @@
 class Solution {
-    static int answer=0;
+    int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        for(int i=0;i<dungeons.length;i++){
+        for(int i=0; i<dungeons.length;i++){
+            if(k<dungeons[i][0]) continue;
             boolean[] visited=new boolean[dungeons.length];
-            dfs(i,k,0,visited,dungeons);
+            dfs(k-dungeons[i][1],i,1,visited,dungeons);
         }
         return answer;
     }
-    public void dfs(int start, int hp, int count, boolean[] visited, int[][] dungeons){
-        visited=visited.clone();
-        if(hp>=dungeons[start][0]&&visited[start]!=true){
-            visited[start]=true;
-            hp-=dungeons[start][1];
-            count++;
-            for(int i=0;i<visited.length;i++){
-                if(visited[i%visited.length]==false)
-                    dfs(i,hp,count,visited,dungeons);
+    public void dfs(int hp,int position, int count,boolean[] visited,int[][] dungeons){
+        visited[position]=true;
+        answer=Math.max(answer,count);
+        for(int i=0;i<dungeons.length;i++){
+            if(!visited[i]&&hp>=dungeons[i][0]){
+                dfs(hp-dungeons[i][1],i,count+1,visited.clone(),dungeons);
             }
         }
-        answer=Math.max(answer,count);
     }
 }
