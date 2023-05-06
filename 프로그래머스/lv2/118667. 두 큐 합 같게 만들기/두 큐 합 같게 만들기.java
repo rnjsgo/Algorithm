3d68ecbe6cf24d5393bcd9a392490c15bb56d7ef
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-05-06 13:45:01 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 53.04 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,43 +1,31 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
         Queue<Integer> queueA=new LinkedList<>();
         Queue<Integer> queueB=new LinkedList<>();
-        long queue1Sum=0;
-        long queue2Sum=0;
-        int maxCount=queue1.length*4;
-        int answer=maxCount;
-        int count=0;
+        long sumA=0, sumB=0;
         for(int i=0;i<queue1.length;i++){
             queueA.add(queue1[i]);
             queueB.add(queue2[i]);
-            queue1Sum+=queue1[i];
-            queue2Sum+=queue2[i];
+            sumA+=queue1[i];
+            sumB+=queue2[i];
         }
-        long half=(queue1Sum+queue2Sum)/2;
-        if((queue1Sum+queue2Sum)%2!=0) return -1;
         
-        while(count<=maxCount){
-            if(queue1Sum==half){
-                answer=count;
-                break;
+        while(answer<1000000){
+            if(sumA>sumB){
+                sumA-=queueA.peek();
+                sumB+=queueA.peek();
+                queueB.add(queueA.poll());
             }
-            if(queue1Sum>half&&queueA.size()>0){
-                int item=queueA.poll();
-                queueB.add(item);
-                queue1Sum-=item;
-                count++;
+            else if(sumA<sumB){
+                sumB-=queueB.peek();
+                sumA+=queueB.peek();
+                queueA.add(queueB.poll());
             }
-            else if(queue1Sum<half&&queueB.size()>0){
-                int item=queueB.poll();
-                queueA.add(item);
-                queue1Sum+=item;
-                count++;
-            }
+            else return answer;
+            answer++;
         }
-        if(answer==maxCount) return -1;
-        return answer;
+        return -1;
     }
-
 }
