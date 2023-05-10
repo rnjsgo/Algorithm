3edb52c41d4e66e4a7b2108eeddd82450a1b9364
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-05-10 11:55:00 +0900

    [unrated] Title: 미로 탈출, Time: 0.36 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/README.md
+++ 프로그래머스/unrated/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [unrated] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
 
 ### 성능 요약
 
-메모리: 69.7 MB, 시간: 0.25 ms
+메모리: 75.8 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
@@ -1,62 +1,61 @@
 import java.util.*;
-class Node{
-    int r,c,distance;
-    Node(int r,int c, int distance){
-        this.r=r;
-        this.c=c;
-        this.distance=distance;
-    }
-}
 class Solution {
-    static Queue<Node> queue=new LinkedList<>();
-    static boolean[][] visited;
-    static Node findNode;
     public int solution(String[] maps) {
+        int answer = 0;
+        Node lever=null;
+        Node exit=null;
         for(int i=0;i<maps.length;i++){
-            boolean isFindStart=false;
-            for(int j=0;j<maps[0].length();j++){
-                if(maps[i].charAt(j)=='S'){
-                    visited=new boolean[maps.length][maps[0].length()];
-                    queue.add(new Node(i,j,0));
-                    bfs('L',maps);
-                    if(findNode.distance==-1) return -1;
-                    isFindStart=true;
-                    break;
-                }
+            for(int j=0;j<maps[i].length();j++){
+                if(maps[i].charAt(j)=='S')
+                    lever=bfs(i,j,'L',maps);
             }
-            if(isFindStart) break;
         }
-        visited=new boolean[maps.length][maps[0].length()];
-        queue=new LinkedList<>();
-        queue.add(findNode);
-        bfs('E',maps);
-        return findNode.distance;
+        if(lever.distance==-1) return -1;
+        exit=bfs(lever.r,lever.c,'E',maps);
+        if(exit.distance==-1) return -1;
+        
+        return lever.distance+exit.distance;
     }
-    public static void bfs(char search,String[] maps){
+    public Node bfs(int r,int c,char destination,String[] maps){
+        Queue<Node> queue=new LinkedList<>();
+        boolean[][] visited=new boolean[maps.length][maps[0].length()];
+        queue.add(new Node(r,c,0));
+        visited[r][c]=true;
+        
         while(!queue.isEmpty()){
             Node node=queue.poll();
             visited[node.r][node.c]=true;
-            if(maps[node.r].charAt(node.c)==search){
-                findNode=node;
-                return;
-            }
+            
+            if(maps[node.r].charAt(node.c)==destination) return node;
+        
             if(node.r>0&&!visited[node.r-1][node.c]&&maps[node.r-1].charAt(node.c)!='X'){
-                queue.add(new Node(node.r-1,node.c,node.distance+1));
                 visited[node.r-1][node.c]=true;
+                queue.add(new Node(node.r-1,node.c,node.distance+1));
             }
             if(node.r<maps.length-1&&!visited[node.r+1][node.c]&&maps[node.r+1].charAt(node.c)!='X'){
-                queue.add(new Node(node.r+1,node.c,node.distance+1));
                 visited[node.r+1][node.c]=true;
+                queue.add(new Node(node.r+1,node.c,node.distance+1));
             }
             if(node.c>0&&!visited[node.r][node.c-1]&&maps[node.r].charAt(node.c-1)!='X'){
-                queue.add(new Node(node.r,node.c-1,node.distance+1));
                 visited[node.r][node.c-1]=true;
+                queue.add(new Node(node.r,node.c-1,node.distance+1));
             }
             if(node.c<maps[0].length()-1&&!visited[node.r][node.c+1]&&maps[node.r].charAt(node.c+1)!='X'){
-                queue.add(new Node(node.r,node.c+1,node.distance+1));
                 visited[node.r][node.c+1]=true;
+                queue.add(new Node(node.r,node.c+1,node.distance+1));
             }
         }
-        findNode=new Node(-1,-1,-1);
+        return new Node(0,0,-1);
+    }
+}
+
+class Node{
+    int r;
+    int c;
+    int distance;
+    Node(int r,int c,int distance){
+        this.r=r;
+        this.c=c;
+        this.distance=distance;
     }
 }
