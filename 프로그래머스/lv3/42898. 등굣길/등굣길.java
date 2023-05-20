dde4e40f6ba1b8815368445d94833f76e2a7ad2f
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-05-20 20:32:14 +0900

    [level 3] Title: 등굣길, Time: 0.76 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.java
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -1,16 +1,18 @@
+
 class Solution {
-    public int solution(int m, int n, int[][] puddles) {
-        int[][] arr=new int[n][m];
-        for(int i=0;i<puddles.length;i++)
-            arr[puddles[i][1]-1][puddles[i][0]-1]=-1;
-        arr[0][0]=1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==-1) continue;
-                if(i>0&&arr[i-1][j]!=-1) arr[i][j]=(arr[i][j]+arr[i-1][j])%1000000007;
-                if(j>0&&arr[i][j-1]!=-1) arr[i][j]=(arr[i][j]+arr[i][j-1])%1000000007;
+    public long solution(int m, int n, int[][] puddles) {
+        long[][] road=new long[n][m];
+        for(int[] p:puddles)
+            road[p[1]-1][p[0]-1]=-1;
+        road[0][0]=1;
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
+                if(road[r][c]==-1) continue;
+                if(r>0&&road[r-1][c]!=-1) road[r][c]+=road[r-1][c];
+                if(c>0&&road[r][c-1]!=-1) road[r][c]+=road[r][c-1];
+                road[r][c]%=1000000007;
             }
         }
-        return arr[n-1][m-1];
+        return road[n-1][m-1];
     }
 }
