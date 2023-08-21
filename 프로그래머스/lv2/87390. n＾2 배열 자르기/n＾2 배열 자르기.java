6490a634287d048d1e9a5f412bd3cccb190e9812
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-08-21 17:27:13 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.76 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,16 +1,10 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
         int[] answer = new int[(int)(right-left+1)];
-        int[] arr=new int[n];
-        int index=0;
-        for(int i=0;i<n;i++)
-            arr[i]=i+1;
-        for(;left<=right;left++){
-            int a=(int)((left)%n);
-            int b=(int)((left)/n);
-            if(a<b) answer[index]=arr[a]+b-a;
-            else answer[index]=arr[a];
-            index++;
+        for(long i=left;i<=right;i++){
+            long c = i/n;
+            long r = i%n;
+            answer[(int)(i-left)] = r<c ? (int)c+1 : (int)r+1 ;
         }
         return answer;
     }
