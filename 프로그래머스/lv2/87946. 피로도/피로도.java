0a55c2a88fae5dfbc6b7b141fd1368cfb50154c5
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-08-21 16:35:14 +0900

    [level 2] Title: 피로도, Time: 0.19 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,19 +1,23 @@
+import java.util.*;
 class Solution {
-    int answer = 0;
+    static int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        for(int i=0; i<dungeons.length;i++){
-            if(k<dungeons[i][0]) continue;
-            boolean[] visited=new boolean[dungeons.length];
-            dfs(k-dungeons[i][1],i,1,visited,dungeons);
+        for(int i=0; i<dungeons.length ; i++){
+            boolean[] visited = new boolean[dungeons.length];
+            dfs(k,i,1,visited,dungeons);
         }
         return answer;
     }
-    public void dfs(int hp,int position, int count,boolean[] visited,int[][] dungeons){
-        visited[position]=true;
-        answer=Math.max(answer,count);
-        for(int i=0;i<dungeons.length;i++){
-            if(!visited[i]&&hp>=dungeons[i][0]){
-                dfs(hp-dungeons[i][1],i,count+1,visited.clone(),dungeons);
+    
+    public void dfs(int health, int index, int depth, boolean[] visited, int[][] dungeons){
+        visited[index] = true;
+        answer = Math.max(answer,depth);
+        health = health - dungeons[index][1];
+        
+        for(int i=0; i<dungeons.length;i++){
+            if(!visited[i] && health >= dungeons[i][0] && health >= dungeons[i][1]){
+                boolean[] visit = visited.clone();
+                dfs(health, i, depth+1, visit, dungeons);
             }
         }
     }
