5d85c22f6aaeb1e202f97b38fd364f6203a56075
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-08-22 19:29:30 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.08 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,45 +1,31 @@
+import java.util.*;
 class Solution {
+    static int answer=0;
+    
     public int solution(int n, int k) {
-        int answer = 0;
-        StringBuilder s=new StringBuilder();
-        int start,end;
-        int index=0;
-        while(n!=0){
-            s.insert(0,n%k);
-            n/=k;
-        }     
-        while(index<s.length()){
-            if(s.charAt(index)!='0'){
-                start=index;
-                StringBuilder a=makeString(start,s);
-                if(checkPrime(a)){
-                    if(start-1<0) answer++;
-                    else if(start+a.length()==s.length()) answer++;
-                    else answer++;
-                }
-                index+=a.length();
-            }
-            else index++;
-        }
+        String num = converter(n,k);
+        String[] split = num.split("0");
+        for(String s : split) isPrime(s);
         return answer;
     }
-    public StringBuilder makeString(int start,StringBuilder s){
-        StringBuilder a=new StringBuilder();
-        while(start<s.length()&&s.charAt(start)!='0'){
-            a.append(s.charAt(start));
-            start++;
+    
+    public String converter(int n, int k){
+        StringBuilder sb = new StringBuilder();
+        while(n>=k){
+            sb.append(n%k);
+            n/=k;
         }
-        return a;
+        sb.append(n);
+        return sb.reverse().toString();
     }
-    public boolean checkPrime(StringBuilder s){
-        long num=Long.parseLong(s.toString());
-        if(num==1) return false;
-        else{
-            for(int i=2;i<=(int)Math.sqrt(num);i++){
-                if(num%i==0)
-                    return false;
-            }
+    
+    public void isPrime(String s){
+        if(s.isEmpty()) return;
+        long num = Long.parseLong(s);
+        if(num==1) return;
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0) return;
         }
-        return true;
+        answer++;
     }
 }
