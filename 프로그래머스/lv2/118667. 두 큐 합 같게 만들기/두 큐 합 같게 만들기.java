24ec4400d18ce8d8285d4e6646bf562e553c5034
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-08-23 22:43:58 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 53.34 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,30 +2,32 @@ import java.util.*;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
-        Queue<Integer> queueA=new LinkedList<>();
-        Queue<Integer> queueB=new LinkedList<>();
-        long sumA=0, sumB=0;
-        for(int i=0;i<queue1.length;i++){
-            queueA.add(queue1[i]);
-            queueB.add(queue2[i]);
+        Queue<Integer> a = new LinkedList<>();
+        Queue<Integer> b = new LinkedList<>();
+        long sumA=0 ,sumB=0;
+        for(int i=0; i<queue1.length; i++){
+            a.add(queue1[i]);
+            b.add(queue2[i]);
             sumA+=queue1[i];
             sumB+=queue2[i];
         }
         
-        while(answer<1000000){
+        while(sumA!=sumB){
+            if(answer>10000000) return -1;
+                
             if(sumA>sumB){
-                sumA-=queueA.peek();
-                sumB+=queueA.peek();
-                queueB.add(queueA.poll());
+                sumB+=a.peek();
+                sumA-=a.peek();
+                b.add(a.poll());
             }
-            else if(sumA<sumB){
-                sumB-=queueB.peek();
-                sumA+=queueB.peek();
-                queueA.add(queueB.poll());
+            else{
+                sumA+=b.peek();
+                sumB-=b.peek();
+                a.add(b.poll());
             }
-            else return answer;
+            
             answer++;
         }
-        return -1;
+        return answer;
     }
 }
