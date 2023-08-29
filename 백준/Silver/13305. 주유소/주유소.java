269606e673eb27e68b9a05362a4ac03d7db7e48c
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-08-29 18:56:52 +0900

    [Silver III] Title: 주유소, Time: 416 ms, Memory: 38096 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int countries = Integer.parseInt(br.readLine());
+        String[] distances = br.readLine().split(" ");
+        String[] prices = br.readLine().split(" ");
+        long sum = 0;
+
+        int lowPrice = Integer.MAX_VALUE;
+        for(int i = 0 ; i<countries-1 ; i++){
+            lowPrice = Math.min(lowPrice, Integer.parseInt(prices[i]));
+            sum += (long)lowPrice * Long.parseLong(distances[i]);
+        }
+        System.out.println(sum);
+    }
+}
