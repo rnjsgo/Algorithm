b75c24443fb9a872700177aab702b59b0eebeab4
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2023-09-01 01:20:14 +0900

    [Silver II] Title: 나무 자르기, Time: 1112 ms, Memory: 139440 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,43 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int need = Integer.parseInt(input[1]);
+        String[] arr = br.readLine().split(" ");
+        int[] trees = new int[arr.length];
+        for(int i=0; i<trees.length; i++){
+            trees[i] = Integer.parseInt(arr[i]);
+        }
+
+        Arrays.sort(trees);
+        long answer= 0 ;
+        long start = 0;
+        long end = trees[trees.length-1];
+        long mid=0;
+        while(start<=end) {
+            mid = (start + end) / 2;
+            long sum = 0;
+            for (int i = 0; i < trees.length; i++) {
+                if (trees[i] > mid) sum += trees[i] - mid;
+            }
+            if (sum > need){
+                answer=mid;
+                start = mid + 1;
+            }
+            else if (sum < need) end = mid - 1;
+            else {
+                answer = mid;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
