bf4a11c67da1db2d19a84782f52b6b5e88ab5fe7
#Super Sun <102651155+rnjsgo@users.noreply.github.com>
2024-03-27 20:29:00 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.53 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,36 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        Queue<Integer> queue = new LinkedList<>();
-        List<int[]> success = new ArrayList<>();
-        long sum = 0;
+        List<int[]> arrList = new ArrayList<>();
+        int sum = 0;
+        int[] result = {0, 0};
         
-        for(int i = 0 ; i < sequence.length ; i++){
-            queue.add(sequence[i]);
+        for(int i=0; i<sequence.length; i++){
+            result[1] = i;
             sum += sequence[i];
-            
-            if(sum > k){
-                while(sum >= k){
-                    if(sum == k){
-                        int[] s = {i-queue.size()+1, i};
-                        success.add(s);
-                        break;
-                    } 
-                    sum -= queue.poll();
+            if(sum < k) continue;
+            else if(sum > k){
+                while(sum > k){
+                    sum -= sequence[result[0]];    
+                    result[0]++;
+                    if(sum == k) arrList.add(result.clone());
                 }
             }
-            else if(sum == k){
-                int[] s = {i-queue.size()+1, i};
-                success.add(s);
-            }
+            else arrList.add(result.clone());
         }
         
-        Collections.sort(success,(a,b)->{
-            int lengthA=(a[1]-a[0]);
-            int lengthB= (b[1]-b[0]);
-            return (lengthA - lengthB == 0) ? a[0] - b[0] : lengthA - lengthB;  
+        Collections.sort(arrList, (a, b)->{
+            int lengthA = a[1] - a[0];
+            int lengthB = b[1] - b[0];
+            return (lengthA == lengthB)? a[0] - b[0] : lengthA - lengthB;
         });
-            
-        return success.get(0);
+
+        return arrList.get(0);
     }
 }
